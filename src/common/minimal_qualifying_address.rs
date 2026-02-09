543bfef0b29aab63659fe953c3fd952305a55dd4
Ruben Nijveld <ruben@tweedegolf.com>
2026-02-09 10:21:10 +0100

    Add tests for minimal qualifying address

--- src/common/minimal_qualifying_address.rs
+++ src/common/minimal_qualifying_address.rs
@@ -14,6 +14,21 @@ pub enum MinimalQualifyingAddress {
     Country(MinimalQualifyingAddressCountry),
 }
 
+impl MinimalQualifyingAddress {
+    /// Creates a new `MinimalQualifyingAddress` as a locality.
+    pub fn new_locality(locality_name: LocalityName) -> Self {
+        MinimalQualifyingAddress::Locality(MinimalQualifyingAddressLocality::new(locality_name))
+    }
+
+    /// Creates a new `MinimalQualifyingAddress` as a country.
+    pub fn new_country(country_name_code: CountryNameCode, locality_name: LocalityName) -> Self {
+        MinimalQualifyingAddress::Country(MinimalQualifyingAddressCountry::new(
+            country_name_code,
+            MinimalQualifyingAddressLocality::new(locality_name),
+        ))
+    }
+}
+
 impl EMLElement for MinimalQualifyingAddress {
     const EML_NAME: QualifiedName<'_, '_> =
         QualifiedName::from_static("QualifyingAddress", Some(NS_EML));
@@ -58,7 +73,16 @@ impl EMLElement for MinimalQualifyingAddress {
     }
 
     fn write_eml(&self, writer: EMLElementWriter) -> Result<(), EMLError> {
-        writer.empty()
+        let mut writer = writer.content()?;
+        match self {
+            MinimalQualifyingAddress::Locality(locality) => {
+                writer = writer.child_elem(MinimalQualifyingAddressLocality::EML_NAME, locality)?
+            }
+            MinimalQualifyingAddress::Country(country) => {
+                writer = writer.child_elem(MinimalQualifyingAddressCountry::EML_NAME, country)?
+            }
+        }
+        writer.finish()
     }
 }
 
@@ -67,6 +91,13 @@ impl EMLElement for MinimalQualifyingAddress {
 pub struct MinimalQualifyingAddressLocality {
     /// Name of the locality
     pub locality_name: LocalityName,
+}
+
+impl MinimalQualifyingAddressLocality {
+    /// Creates a new `MinimalQualifyingAddressLocality` with the given locality name.
+    pub fn new(locality_name: LocalityName) -> Self {
+        MinimalQualifyingAddressLocality { locality_name }
+    }
 }
 
 impl EMLElement for MinimalQualifyingAddressLocality {
@@ -97,6 +128,19 @@ pub struct MinimalQualifyingAddressCountry {
     pub locality: MinimalQualifyingAddressLocality,
 }
 
+impl MinimalQualifyingAddressCountry {
+    /// Creates a new `MinimalQualifyingAddressCountry` with the given country name code and locality.
+    pub fn new(
+        country_name_code: CountryNameCode,
+        locality: MinimalQualifyingAddressLocality,
+    ) -> Self {
+        MinimalQualifyingAddressCountry {
+            country_name_code,
+            locality,
+        }
+    }
+}
+
 impl EMLElement for MinimalQualifyingAddressCountry {
     const EML_NAME: QualifiedName<'_, '_> = QualifiedName::from_static("Country", Some(NS_XAL));
 
@@ -112,5 +156,86 @@ impl EMLElement for MinimalQualifyingAddressCountry {
             .child_elem(CountryNameCode::EML_NAME, &self.country_name_code)?
             .child_elem(MinimalQualifyingAddressLocality::EML_NAME, &self.locality)?
             .finish()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::io::{EMLParsingMode, EMLRead as _, test_write_eml_element, test_xml_fragment};
+
+    use super::*;
+
+    #[test]
+    fn test_minimal_qualifying_address_construction() {
+        let locality_name = LocalityName::new("Amsterdam");
+        let locality =
+            MinimalQualifyingAddress::new_country(CountryNameCode::new("NL"), locality_name);
+
+        if let MinimalQualifyingAddress::Country(country) = locality {
+            assert_eq!(country.country_name_code.value, "NL");
+            assert_eq!(country.locality.locality_name.name, "Amsterdam");
+        } else {
+            panic!("Expected a country qualifying address");
+        }
+
+        let locality_name = LocalityName::new("Rotterdam");
+        let locality = MinimalQualifyingAddress::new_locality(locality_name);
+        if let MinimalQualifyingAddress::Locality(locality) = locality {
+            assert_eq!(locality.locality_name.name, "Rotterdam");
+        } else {
+            panic!("Expected a locality qualifying address");
+        }
+    }
+
+    #[test]
+    fn test_minimal_qualifying_address_country_parsing() {
+        let xml = test_xml_fragment(
+            r#"
+            <QualifyingAddress xmlns="urn:oasis:names:tc:evs:schema:eml" xmlns:xal="urn:oasis:names:tc:ciq:xsdschema:xAL:2.0">
+                <xal:Country>
+                    <xal:CountryNameCode>NL</xal:CountryNameCode>
+                    <xal:Locality>
+                        <xal:LocalityName>Amsterdam</xal:LocalityName>
+                    </xal:Locality>
+                </xal:Country>
+            </QualifyingAddress>
+            "#,
+        );
+
+        let address = MinimalQualifyingAddress::parse_eml(&xml, EMLParsingMode::Strict).unwrap();
+
+        if let MinimalQualifyingAddress::Country(country) = &address {
+            assert_eq!(country.country_name_code.value, "NL");
+            assert_eq!(country.locality.locality_name.name, "Amsterdam");
+        } else {
+            panic!("Expected a country qualifying address");
+        }
+
+        let xml_output = test_write_eml_element(&address, &[NS_EML, NS_XAL]).unwrap();
+        assert_eq!(xml_output, xml);
+    }
+
+    #[test]
+    fn test_minimal_qualifying_address_locality_parsing() {
+        let xml = test_xml_fragment(
+            r#"
+            <QualifyingAddress xmlns="urn:oasis:names:tc:evs:schema:eml" xmlns:xal="urn:oasis:names:tc:ciq:xsdschema:xAL:2.0">
+                <xal:Locality>
+                    <xal:LocalityName>Amsterdam</xal:LocalityName>
+                </xal:Locality>
+            </QualifyingAddress>
+            "#,
+        );
+
+        let address = MinimalQualifyingAddress::parse_eml(&xml, EMLParsingMode::Strict).unwrap();
+
+        if let MinimalQualifyingAddress::Locality(locality) = &address {
+            assert_eq!(locality.locality_name.name, "Amsterdam");
+        } else {
+            panic!("Expected a locality qualifying address");
+        }
+
+        let xml_output = test_write_eml_element(&address, &[NS_EML, NS_XAL]).unwrap();
+        assert_eq!(xml_output, xml);
     }
 }
