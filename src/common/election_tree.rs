bf53323c9f141c14528d2255128fb38bf2a6e4b6
Tikitikitikidesuka <deesneakygerbil@gmail.com>
2026-05-22 00:17:44 +0200

    Update ElectionTree and add tests

--- src/common/election_tree.rs
+++ src/common/election_tree.rs
@@ -1,3 +1,4 @@
+use crate::common::region::Region;
 use crate::{
     EMLError, NS_KR,
     io::{EMLElement, EMLElementReader, EMLElementWriter, QualifiedName, collect_struct},
@@ -7,20 +8,20 @@ use crate::{
 #[derive(Debug, Clone)]
 pub struct ElectionTree {
     /// Regions defined for this part of the election tree
-    pub regions: Vec<ElectionTreeRegion>,
+    pub regions: Vec<Region>,
 }
 
 impl ElectionTree {
     /// Create a new election tree with the given regions.
-    pub fn new(regions: impl Into<Vec<ElectionTreeRegion>>) -> Self {
+    pub fn new(regions: impl Into<Vec<Region>>) -> Self {
         ElectionTree {
             regions: regions.into(),
         }
     }
 }
 
-impl From<Vec<ElectionTreeRegion>> for ElectionTree {
-    fn from(regions: Vec<ElectionTreeRegion>) -> Self {
+impl From<Vec<Region>> for ElectionTree {
+    fn from(regions: Vec<Region>) -> Self {
         ElectionTree::new(regions)
     }
 }
@@ -30,32 +31,97 @@ impl EMLElement for ElectionTree {
 
     fn read_eml(elem: &mut EMLElementReader<'_, '_>) -> Result<Self, EMLError> {
         Ok(collect_struct!(elem, ElectionTree {
-            regions as Vec: ElectionTreeRegion::EML_NAME => |elem| ElectionTreeRegion::read_eml(elem)?,
+            regions as Vec: Region::EML_NAME => |elem| Region::read_eml(elem)?,
         }))
     }
 
     fn write_eml(&self, writer: EMLElementWriter) -> Result<(), EMLError> {
         writer
-            .child_elems(ElectionTreeRegion::EML_NAME, &self.regions)?
+            .child_elems(Region::EML_NAME, &self.regions)?
             .finish()
     }
 }
 
-/// A region in the election tree.
-#[derive(Debug, Clone)]
-pub struct ElectionTreeRegion {}
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::common::committee_category::CommitteeCategory;
+    use crate::common::region_category::RegionCategory;
+    use crate::io::{EMLParsingMode, EMLRead, test_write_eml_element, test_xml_fragment};
 
-impl EMLElement for ElectionTreeRegion {
-    const EML_NAME: QualifiedName<'_, '_> = QualifiedName::from_static("Region", Some(NS_KR));
+    #[test]
+    fn test_election_tree_parsing() {
+        let xml = test_xml_fragment(
+            r#"
+            <kr:ElectionTree xmlns:kr="http://www.kiesraad.nl/extensions">
+                <kr:Region RegionCategory="STAAT" RomanNumerals="false" FrysianExportAllowed="false">
+                    <kr:RegionName>Region 1</kr:RegionName>
+                </kr:Region>
+                <kr:Region RegionNumber="1" RegionCategory="PROVINCIE" RomanNumerals="false" FrysianExportAllowed="false" SuperiorRegionCategory="STAAT">
+                    <kr:RegionName>Region 2</kr:RegionName>
+                    <kr:Committee CommitteeCategory="HSB"/>
+                </kr:Region>
+                <kr:Region RegionNumber="2" RegionCategory="GEMEENTE" RomanNumerals="false" FrysianExportAllowed="false" SuperiorRegionCategory="PROVINCIE">
+                    <kr:RegionName>Region 3</kr:RegionName>
+                    <kr:Committee CommitteeCategory="CSB"/>
+                </kr:Region>
+                <kr:Region RegionNumber="3" RegionCategory="KIESKRING" RomanNumerals="false" FrysianExportAllowed="false" SuperiorRegionCategory="GEMEENTE">
+                    <kr:RegionName>Region 4</kr:RegionName>
+                    <kr:Committee CommitteeCategory="PSB"/>
+                </kr:Region>
+            </kr:ElectionTree>
+            "#,
+        );
 
-    fn read_eml(elem: &mut EMLElementReader<'_, '_>) -> Result<Self, EMLError> {
-        // TODO: handle election tree region
-        elem.skip()?;
-        Ok(ElectionTreeRegion {})
-    }
+        let tree = ElectionTree::parse_eml(&xml, EMLParsingMode::Strict).unwrap();
+        assert_eq!(tree.regions.len(), 4);
 
-    fn write_eml(&self, writer: EMLElementWriter) -> Result<(), EMLError> {
-        // TODO: write election tree region
-        writer.empty()
+        assert_eq!(tree.regions[0].name, "Region 1");
+        assert_eq!(tree.regions[0].category, RegionCategory::STAAT);
+        assert_eq!(tree.regions[0].number, None);
+        assert_eq!(tree.regions[0].superior_region_category, None);
+        assert!(tree.regions[0].committees.is_empty());
+
+        assert_eq!(tree.regions[1].name, "Region 2");
+        assert_eq!(tree.regions[1].number, Some(1));
+        assert_eq!(tree.regions[1].category, RegionCategory::PROVINCIE);
+        assert_eq!(
+            tree.regions[1].superior_region_category,
+            Some(RegionCategory::STAAT)
+        );
+        assert_eq!(tree.regions[1].committees.len(), 1);
+        assert_eq!(
+            tree.regions[1].committees[0].category,
+            CommitteeCategory::HSB
+        );
+
+        assert_eq!(tree.regions[2].name, "Region 3");
+        assert_eq!(tree.regions[2].number, Some(2));
+        assert_eq!(tree.regions[2].category, RegionCategory::GEMEENTE);
+        assert_eq!(
+            tree.regions[2].superior_region_category,
+            Some(RegionCategory::PROVINCIE)
+        );
+        assert_eq!(tree.regions[2].committees.len(), 1);
+        assert_eq!(
+            tree.regions[2].committees[0].category,
+            CommitteeCategory::CSB
+        );
+
+        assert_eq!(tree.regions[3].name, "Region 4");
+        assert_eq!(tree.regions[3].number, Some(3));
+        assert_eq!(tree.regions[3].category, RegionCategory::KIESKRING);
+        assert_eq!(
+            tree.regions[3].superior_region_category,
+            Some(RegionCategory::GEMEENTE)
+        );
+        assert_eq!(tree.regions[3].committees.len(), 1);
+        assert_eq!(
+            tree.regions[3].committees[0].category,
+            CommitteeCategory::PSB
+        );
+
+        let xml_output = test_write_eml_element(&tree, &[NS_KR]).unwrap();
+        assert_eq!(xml_output, xml);
     }
 }
